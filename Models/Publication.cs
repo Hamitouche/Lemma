55659f54c9e4ed2eb7c659061cde5d969f71b0f5
Hamitouche Thinhinane <ft_hamitouche@esi.dz>
2018-10-27 07:44:54 +0200

    implémenter les models final

--- Models/Publication.cs
+++ Models/Publication.cs
@@ -6,10 +6,41 @@ namespace LemmaApp.Models
 {
     public class Publication
     {
-        public string Id { get; set; }
-        public string contenu { get; set; }
-        public string pieceJoint { get; set; }
-        public string description{ get; set; }
-        //public Auteur auteur { get; set; }
+
+        /*public int id { get; set; }
+        public int id_user { get; set; }
+        public int id_famille { get; set; }*/
+        public string description { get; set; }
+        public DateTime date_post { get; set; }
+
+        public int var { get; set; }
+        public string imgUrl { get; set; }
+
+        public List<Publication> getPub()
+        {
+            List<Publication> pubs = new List<Publication>
+            {
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+                new Publication() {description="description1", date_post=DateTime.Now, var=0, imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTWg_U0HGK7FIS7PCxqbRSmuZBpcebJepQeGig4xn2PDNuptyrqr0z2M64nw"},
+
+            };
+            return pubs;
+        }
     }
 }
+
+    
