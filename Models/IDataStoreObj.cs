148770929948f10f4ff36c4b2ba4573749eef022
Hamitouche Thinhinane <ft_hamitouche@esi.dz>
2018-10-27 06:21:55 +0200

    Implémenter toutes les classes de Models.

---
+++ Models/IDataStoreObj.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LemmaApp.Services
+{
+    public interface IDataStoreObj<F>
+    {
+        Task<bool> AddFamilleAsync(F famille);
+        Task<bool> UpdateFamilleAsync(F famille);
+        //Task<bool> DeleteFamilleAsync(F famille);
+        Task<F> GetFamilleAsync(string id);
+        Task<IEnumerable<F>> GetFamilleAsync(bool forceRefresh = false);
+    }
+}
---
+++ Models/LemmaManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using LemmaApp.Models;
+[assembly: Xamarin.Forms.Dependency(typeof(LemmaApp.Services.LemmaManager))]
+namespace LemmaApp.Services
+{
+    class LemmaManager
+    {
+        
+        
+            List<Lemma> lemmas;
+            public LemmaManager()
+            {
+                lemmas = new List<Lemma>();
+                var mockItems = new List<Lemma>
+                {
+                    /* new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
+                     new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
+                     new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
+                     new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
+                     */
+                };
+
+                foreach (var item in mockItems)
+                {
+                lemmas.Add(item);
+                }
+            }
+
+            public async Task<bool> AddLemmaAsync(Lemma lemma)
+            {
+                  lemmas.Add(lemma);
+
+                return await Task.FromResult(true);
+            }
+
+            public async Task<bool> UpdateLemmaAsync(Lemma lemma)
+            {
+
+
+                return await Task.FromResult(true);
+            }
+
+            public async Task<bool> CancelLemmaAsync(Lemma item)
+            {
+
+
+                return await Task.FromResult(true);
+            }
+
+            public async Task<Lemma> GetLemmaAsync(string id)
+            {
+                return await Task.FromResult(lemmas.FirstOrDefault(s => s.Id == id));
+            }
+
+            public async Task<IEnumerable<Lemma>> GetLemmaAsync(bool forceRefresh = false)
+            {
+                return await Task.FromResult(lemmas);
+            }
+            public async Task<bool> ActiverModeLemmaAsync()
+            {
+              return await Task.FromResult(true);
+            }
+            public async Task<bool> AcceptLemmaAsync(Lemma item)
+        {
+
+
+            return await Task.FromResult(true);
+        }
+    }
+    }
+
